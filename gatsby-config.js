42643bff83f13d0ce39829a5c7225b14c000983c
Cliff Wulfman <cwulfman@princeton.edu>
2021-06-24 15:38:55 -0400

    adding airtable filesystem plugins; switching to yarn

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,3 +1,19 @@
 module.exports = {
-  plugins: ["gatsby-plugin-gatsby-cloud"],
+    plugins: [
+	"gatsby-plugin-gatsby-cloud",
+	{
+	    resolve: `gatsby-source-airtable`,
+	    options: {
+		apiKey: process.env.GATSBY_AIRTABLE_API_KEY,
+		concurrency: 5,
+		tables: [
+		    {
+			baseId: process.env.BDDA_BASE_ID,
+			tableName: "Appellation",
+
+		    },
+		],
+	    },
+	},
+    ],
 };
